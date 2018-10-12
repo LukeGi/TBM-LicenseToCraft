f6472c8594bf9187ca1fecedd8af3ee626887a5a
Blue_Monster122 <boo122333@gmail.com>
2018-10-12 17:56:02 +0100

    Upgrades Profiler
    Makes the profiler keep track of how long the mod has spent profiling it's loading, and when the game has loaded print to console how long the mod took to load.

--- src/main/java/tbm/licensetocraft/util/Profiler.java
+++ src/main/java/tbm/licensetocraft/util/Profiler.java
@@ -2,6 +2,16 @@ package tbm.licensetocraft.util;
 
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
 import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,15 +20,17 @@ import tbm.licensetocraft.LicenseToCraft;
 public final class Profiler {
     private static final Object2LongMap<String> sections = new Object2LongOpenHashMap<>();
     private static final Logger LOGGER = LogManager.getLogger(LicenseToCraft.ID + "-profiler");
+    private static long loadingTime = 0L;
 
-    private Profiler() {
+    static {
+        MinecraftForge.EVENT_BUS.register(EventHandler.class);
         if (!LicenseToCraft.isDevEnv()) {
             ((org.apache.logging.log4j.core.Logger) LOGGER).setLevel(Level.OFF);
         }
     }
 
     public static void start(String section) {
-        sections.put(section, System.currentTimeMillis());
+        sections.put(section, System.nanoTime());
         LOGGER.info(String.format("Starting Section [%s]", section));
     }
 
@@ -27,7 +39,20 @@ public final class Profiler {
         if (start == 0) {
             LOGGER.error(String.format("Attempted to end a section, [%s], in profiler which is not yet created", section));
         } else {
-            LOGGER.info(String.format("Ending Section [%s], took %d ms", section, (System.currentTimeMillis() - start)));
+            long time = (System.nanoTime() - start);
+            if (sections.isEmpty()) loadingTime += time;
+            LOGGER.info(String.format("Ending Section [%s], took %d ms", section, (time / 1000000)));
+        }
+    }
+
+    public static class EventHandler {
+        @SubscribeEvent
+        @SideOnly(Side.CLIENT)
+        public static void onGuiOpen(GuiOpenEvent event) {
+            if (event.getGui() instanceof GuiMainMenu) {
+                LicenseToCraft.LOGGER.info(String.format("Took %d ms to load.", (loadingTime / 1000000)));
+                MinecraftForge.EVENT_BUS.unregister(EventHandler.class);
+            }
         }
     }
 }
